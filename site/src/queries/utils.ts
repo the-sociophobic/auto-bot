3651d69d1980e227a72e363d2cffa88be8c3c480
the-sociophobic <torikowashi@gmail.com>
2024-04-09 20:34:34 +0300

    add gh-pages

--- site/src/queries/utils.ts
+++ site/src/queries/utils.ts
@@ -3,8 +3,8 @@ import { getAuthHeader } from '../hooks/auth'
 
 
 // const REACT_APP_SERVER_URL = 'https://shark-app-p76lu.ondigitalocean.app/api'
-const REACT_APP_SERVER_URL = 'http://localhost:5005'
-// const REACT_APP_SERVER_URL = 'https://hyperdao.xyz'
+// const REACT_APP_SERVER_URL = 'http://localhost:5005'
+const REACT_APP_SERVER_URL = 'https://hyperdao.xyz'
 
 
 export const generateURL = (path: string, params?: object): string =>
